b9209272f96e0c38c5ce983bac4862a3b2f9ad84
arecibo <arecibo@9fd78695-1745-4fa9-81b8-4a40a47fd399>
2008-09-12 19:14:25 +0000

    first pass not working yet
    git-svn-id: http://arecibo.svn.beanstalkapp.com/clients/trunk@4 9fd78695-1745-4fa9-81b8-4a40a47fd399

---
+++ plone/clearwind.arecibo/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.2'
+
+setup(name='clearwind.arecibo',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='ClearWind Consulting',
+      author_email='andy@clearwind.ca',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['clearwind'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
