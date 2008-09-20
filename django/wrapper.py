18c57316ac73e9aea14079d1804d2af414dba279
arecibo <arecibo@9fd78695-1745-4fa9-81b8-4a40a47fd399>
2008-09-20 15:09:08 +0000

    fix urls
    git-svn-id: http://arecibo.svn.beanstalkapp.com/clients/trunk@19 9fd78695-1745-4fa9-81b8-4a40a47fd399

--- django/wrapper.py
+++ django/wrapper.py
@@ -11,9 +11,12 @@ import time
 
 def post(request, status, **kw):
     exc_info = sys.exc_info() 
+    url = "%s://%s%s" % (request.META["wsgi.url_scheme"],
+            request.get_host(),
+            request.get_full_path())
     data = {
         "account": settings.ARECIBO_PUBLIC_ACCOUNT_NUMBER,
-        "url": request.get_full_path(),
+        "url": url, 
         "ip": request.META.get('REMOTE_ADDR'),
         "traceback": "\n".join(traceback.format_tb(exc_info[2])),
         "type": exc_info[0],
