4d7aa217a1691aa3e53c2a14d82c3c397f78b60e
arecibo <arecibo@9fd78695-1745-4fa9-81b8-4a40a47fd399>
2008-09-20 15:14:53 +0000

    better method
    git-svn-id: http://arecibo.svn.beanstalkapp.com/clients/trunk@20 9fd78695-1745-4fa9-81b8-4a40a47fd399

--- django/wrapper.py
+++ django/wrapper.py
@@ -11,12 +11,9 @@ import time
 
 def post(request, status, **kw):
     exc_info = sys.exc_info() 
-    url = "%s://%s%s" % (request.META["wsgi.url_scheme"],
-            request.get_host(),
-            request.get_full_path())
     data = {
         "account": settings.ARECIBO_PUBLIC_ACCOUNT_NUMBER,
-        "url": url, 
+        "url": request.build_absolute_uri(), 
         "ip": request.META.get('REMOTE_ADDR'),
         "traceback": "\n".join(traceback.format_tb(exc_info[2])),
         "type": exc_info[0],
