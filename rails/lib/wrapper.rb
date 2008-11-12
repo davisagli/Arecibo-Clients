0db716e086724eb88cf54fed34503697fa350f16
arecibo <arecibo@9fd78695-1745-4fa9-81b8-4a40a47fd399>
2008-11-12 12:32:55 +0000

    add in error page
    git-svn-id: http://arecibo.svn.beanstalkapp.com/clients/trunk@35 9fd78695-1745-4fa9-81b8-4a40a47fd399

--- rails/lib/wrapper.rb
+++ rails/lib/wrapper.rb
@@ -10,9 +10,16 @@ class ActionController::Base
      end
   end
   
+  def override_arecibo exception
+      # this is a method once all the data
+      # has been prepared for the default setup
+      # if you would like to change it now is
+      # good chance to do so, eg: change priority
+      # or exclude some kinds of errors, or certain ips
+  end
   
   def report_to_arecibo exception
-    data = {
+    @arecibodata = {
       :account => ARECIBO_ACCOUNT_NUMBER,
       :msg => "",
       :url => "#{request.protocol}#{request.host_with_port}#{request.request_uri}",
@@ -24,19 +31,32 @@ class ActionController::Base
     
     name = exception.class.name
     if name == "ActionController::RoutingError"
-        data[:status] = 404
-        data[:priority] = 5
+        @arecibodata[:status] = 404
+        @arecibodata[:priority] = 5
     else
-        data[:status] = 500
-        data[:priority] = 1
+        @arecibodata[:status] = 500
+        @arecibodata[:priority] = 1
     end
     
-    data[:type] = exception.class.name
-    data[:traceback] = "#{exception.backtrace.join("\n")}"
-    # make a nice message out of the post data
-    request.env.each { |k, v| data[:msg] += "#{k.to_s}: #{v.to_s}\n"  }
+    @arecibodata[:type] = exception.class.name
+    @arecibodata[:traceback] = "#{exception.backtrace.join("\n")}"
+    # make a nice message out of the post @arecibodata
+    request.env.each { |k, v| @arecibodata[:msg] += "#{k.to_s}: #{v.to_s}\n"  }
     
-    p = Arecibo.new(data)
-    p.send
+    override_arecibo(exception)
+    
+    begin 
+      p = Arecibo.new(@arecibodata)
+      p.send
+    rescue 
+      logger.error "Posting to Arecibo failed with: #{$!} for error #{@arecibodata[:type]}"
+    end
+    
+    begin
+      render :template => ARECIBO_RESULT_TEMPLATE, 
+             :status => @arecibodata[:status]
+    rescue
+      # pass, if ARECIBO_RESULT_TEMPLATE fails, ignore this
+    end
   end
 end
